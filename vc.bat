81abdf6edce74b07012d2cf04c00ee35f8d5aae4
Hannes Domani <ssbssa@yahoo.de>
2014-09-17 14:44:35 +0200

    fix dll-loading example for VC

--- vc.bat
+++ vc.bat
@@ -1,11 +1,15 @@
+
+cl dll-alloc.c /LD /MD /Zi
+copy dll-alloc.dll dll-alloc-shared.dll
+
 if "%1" == "vc6" goto vc6
 
-cl allocer.cpp /MD /Zi
+cl allocer.cpp /MD /Zi dll-alloc.lib
 mt.exe -manifest allocer.exe.manifest -outputresource:allocer.exe;1
 goto eof
 
 :vc6
 lib /def:crt32-vc6.def /out:crt32-vc6.lib
-cl allocer.cpp /MD /Zi crt32-vc6.lib
+cl allocer.cpp /MD /Zi crt32-vc6.lib dll-alloc.lib
 
 :eof
