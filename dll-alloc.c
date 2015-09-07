d41e8fd17c2f9feb1afbadb029ff97cf69dc1635
Hannes Domani <ssbssa@yahoo.de>
2015-09-07 19:09:25 +0200

    update copyright year range

--- dll-alloc.c
+++ dll-alloc.c
@@ -1,4 +1,9 @@
 
+//          Copyright Hannes Domani 2014 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <stdlib.h>
 #include <windows.h>
 
