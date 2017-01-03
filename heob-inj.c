efc903ade4ebf19d553098a415b33186ae570bb5
Hannes Domani <ssbssa@yahoo.de>
2017-01-03 18:47:42 +0100

    only decommit memory if it is not released

--- heob-inj.c
+++ heob-inj.c
@@ -1,5 +1,5 @@
 
-//          Copyright Hannes Domani 2014 - 2016.
+//          Copyright Hannes Domani 2014 - 2017.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -1935,10 +1935,10 @@ static NOINLINE void protect_free_m( void *b,funcType ft )
 
   b = (void*)p;
 
-  VirtualFree( b,pages*pageSize,MEM_DECOMMIT );
-
   if( !rd->opt.protectFree )
     VirtualFree( b,0,MEM_RELEASE );
+  else
+    VirtualFree( b,pages*pageSize,MEM_DECOMMIT );
 }
 
 // }}}
