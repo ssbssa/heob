7805cfa0b2bd717ed8928e6f9bea4de79fef5405
Hannes Domani <ssbssa@yahoo.de>
2023-02-23 16:40:59 +0100

    update copyright years

--- svg.js
+++ svg.js
@@ -1,3 +1,9 @@
+
+//          Copyright Hannes Domani 2018-2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 const SHOW_SUM_COUNT = 100;
 
 var headerHeight = 50;
