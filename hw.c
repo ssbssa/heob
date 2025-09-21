be126db148fc4987570b53cc932a10fc87388b39
Hannes Domani <ssbssa@yahoo.de>
2025-09-21 13:20:36 +0200

    add hardware breakpoint test

---
+++ hw.c
@@ -0,0 +1,57 @@
+
+int func_1()
+{
+  return 1;
+}
+
+int func_2()
+{
+  return 2;
+}
+
+int func_3()
+{
+  return 3;
+}
+
+int func_4()
+{
+  return 4;
+}
+
+int func_5()
+{
+  return 5;
+}
+
+int func_6()
+{
+  return 6;
+}
+
+int func_7()
+{
+  return 7;
+}
+
+int func_8()
+{
+  return 8;
+}
+
+int main()
+{
+  int num_1 = 0, num_2 = 0, num_3 = 0, num_4 = 0;
+  int num_5 = 0, num_6 = 0, num_7 = 0, num_8 = 0;
+
+  num_1 = func_1();
+  num_2 = func_2();
+  num_3 = func_3();
+  num_4 = func_4();
+  num_5 = func_5();
+  num_6 = func_6();
+  num_7 = func_7();
+  num_8 = func_8();
+
+  return num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7 + num_8;
+}
